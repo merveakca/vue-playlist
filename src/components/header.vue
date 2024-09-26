94d691dcd88ee84622e71b4982185e5d642dbdce
merveakca <smkirac@gmail.com>
2024-09-26 22:26:36 +0300

    first commit

---
+++ src/components/header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+      <h1>Kişiler ve Meslekleri</h1>
+    </header>
+</template>
+
+<script>
+export default{
+  data(){
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+header{
+  background: rgb(144, 186, 238);
+  padding: 10px;
+}
+h1{
+  color: #0d0834;
+  text-align: center;
+}
+</style>
